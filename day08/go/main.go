c437f3835d249249c956cd3541146a7fbea2d4e1
Gergen <bryan.gergen@nytimes.com>
2020-01-21 23:26:49 -0500

    Day8 Go Part2

--- day08/go/main.go
+++ day08/go/main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strconv"
+	"strings"
+)
+
+const (
+	black = iota
+	white
+	transparent
 )
 
 func main() {
@@ -18,8 +25,16 @@ func main() {
 		panic(err)
 	}
 
+	d, err := decodeImage(n, 25, 6)
+	if err != nil {
+		panic(err)
+	}
+
 	// Part 1
 	fmt.Println(v)
+
+	// Part 2
+	fmt.Println(translateDecoding(d))
 }
 
 type Count struct {
@@ -54,6 +69,52 @@ func checkFidelity(t string, w, h int) (int, error) {
 	}
 
 	return count.ones * count.twos, nil
+}
+
+func decodeImage(t string, w, h int) ([][]int, error) {
+	layers := getSlices(t, w*h)
+	layersByRows := make([][]string, len(layers))
+	for i, l := range layers {
+		layersByRows[i] = getSlices(l, w)
+	}
+
+	d := make([][]int, h)
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			if i == 0 {
+				d[j] = make([]int, w)
+			}
+			for k, pixelFound := 0, false; k < len(layersByRows) && !pixelFound; k++ {
+				v, err := strconv.Atoi(string(layersByRows[k][j][i]))
+				if err != nil {
+					return nil, err
+				} else if v != transparent {
+					d[j][i] = v
+					pixelFound = true
+				}
+			}
+		}
+	}
+
+	return d, nil
+}
+
+func translateDecoding(img [][]int) string {
+	var t strings.Builder
+	for _, row := range img {
+		var r strings.Builder
+		for _, pixel := range row {
+			var p string
+			if pixel == black {
+				p = "\u25A0"
+			} else {
+				p = "\u25A1"
+			}
+			r.WriteString(p)
+		}
+		t.WriteString(r.String() + "\n")
+	}
+	return t.String()
 }
 
 func getSlices(s string, ls int) []string {
